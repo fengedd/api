8fa12af2bbb3105a84c265ccf20007cb30742716
fengedd <fengedd@gmail.com>
2019-01-10 14:23:14 -0800

    look up player query

--- src/store/queries.js
+++ src/store/queries.js
@@ -2,18 +2,39 @@ const knex = require('../db');
 const util = require('util');
 const db = knex.db;
 
-// function upsert(db, table, row, conflict) {}
-function insertPlayer(player, options, cb) {
+function insertPlayer(player) {
   const table = 'players';
-  const data = { id: 611920, name: 'whothesssi it' };
+  const data = player;
   const insert = db(table).insert(data);
   const update = db.queryBuilder().update(data);
   const res = db
     .raw(`? ON CONFLICT (id) DO ? returning *`, [insert, update])
     .get('rows')
     .get(0)
-    .then(val => console.log(val))
     .finally(() => db.destroy());
+  return res;
 }
 
-insertPlayer(null, null, null);
+function getPlayer(id) {
+  const table = 'players';
+  const data = id;
+  const query = db(table)
+    .select('*')
+    .where('id', data)
+    .toQuery();
+  const res = db
+    .raw(query)
+    .get('rows')
+    .get(0)
+    .finally(() => db.destroy());
+  return res;
+}
+
+const caller = async () => {
+  const result = await getPlayer(690); //insertPlayer(null, null, null);
+  console.log(result);
+};
+
+caller();
+
+// insertPlayer(null, null, null);
